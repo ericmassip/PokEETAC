91c08a8553e0d6ea52e72ec37543da3e7d1351cc
Samuel Segura <samuel3107@gmail.com>
2017-01-21 01:25:06 +0100

    image upload v2

--- src/main/java/serviceLibrary/ProfemonService.java
+++ src/main/java/serviceLibrary/ProfemonService.java
@@ -3,12 +3,17 @@ package serviceLibrary;
 import business.LocationBusiness;
 import business.ProfemonBusiness;
 import entity.Profemon;
+import entity.serviceLibraryResults.ImageResult;
 import entity.serviceLibraryResults.ProfemonLocationResult;
 import infrastructure.ProfemonRepository;
+import org.apache.commons.codec.binary.Base64;
 import org.apache.log4j.Logger;
 
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
+import java.awt.*;
+import java.io.FileOutputStream;
+import java.io.OutputStream;
 import java.util.List;
 
 /**
@@ -37,6 +42,21 @@ public class ProfemonService {
     public void insertProfemon (Profemon profemon) {
         ProfemonRepository profemonRepository = new ProfemonRepository();
         profemonRepository.insertProfemon(profemon);
+    }
+
+    @POST
+    @Path("/image")
+    public void saveImage (ImageResult imageResult ) {
+
+        byte[] data = Base64.decodeBase64(
+                "Aqui es donde va la string gigante," +
+                        " pero si la metes aqui a pelo te dará error porque es demasiado grande");
+        try (OutputStream stream = new FileOutputStream("path/to/profedex/nombre.png")) {
+            stream.write(data);
+        }
+        catch(Exception e){
+            log.info(e.getMessage());
+        }
     }
 
     @DELETE
